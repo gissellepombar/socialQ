42189bb69022587907213220556cc2f291faa7df
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-02-14 16:31:45 -0500

    updated styling

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
+import { createContext, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Feed from './src/components/Feed'
+import Feed from './src/components/Feed';
+import Details from './src/components/Details'
+
+const Stack = createNativeStackNavigator();
+export const PostContext = createContext();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Feed />
-      <StatusBar style="auto" />
-    </View>
+    const [currentPost, setCurrentPost] = useState();
+    return (
+    <PostContext.Provider value={{currentPost, setCurrentPost}}>
+        <NavigationContainer>
+            <StatusBar style="auto" />
+                <Stack.Navigator initialRouteName='Home'>
+                    <Stack.Screen name="Home" component={Feed} />
+                    <Stack.Screen name="Details" component={Details} />
+                </Stack.Navigator>
+        </NavigationContainer>
+    </PostContext.Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,30 +1,50 @@
-import { Image, Text, View, StyleSheet} from 'react-native';
+import { useContext } from "react";
+import { Image, Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { PostContext } from "../../App";
 
-export default function Post({ post }) {
-    return (
-        <View style={styles.post}>
-            <Image style={styles.image} source={{ uri: post.photo}} />
-            <Text style={styles.title}>{post.description}</Text>
-        </View>
-    )
+export default function Posts({ post, navigation }) {
+  const { setCurrentPost } = useContext(PostContext);
+  const handleTouch = () => {
+    setCurrentPost(post)
+    navigation.navigate("Details")
+  }
+  return (
+    <TouchableOpacity onPress={handleTouch}>
+      <View style={styles.post}>
+        <Image style={styles.image} source={{ uri: post.photo }} />
+        <Text style={styles.title}>{post.description}</Text>
+      </View>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
-    post: {
-        margin: 10, 
-        padding: 10,
-        borderRadius: 10, 
-        backgroundColor: 'white', 
-        shadowColor: 'black', 
-        shadowOffset: { width: 1, height: 2}, 
-    }, 
-    image: {
-        width: "100%", 
-        height: 300,
-        marginBottom: 10,
-        borderRadius: 10,
+  post: {
+    margin: 30,
+    padding: 13,
+    borderRadius: 10,
+    backgroundColor: 'rgba(95,94,130,0.8)',
+    
+    shadowOffset: {
+      width: "0",
+      height: "0",
     },
-    title: {
-        fontSize: 20,
-    },
+    shadowOpacity: "0.96",
+    shadowRadius: "10",
+    shadowColor: 'black'
+  },
+
+  image: {
+    width: '100%',
+    borderRadius: 10,
+    marginBottom: 10,
+    height: 300,
+  },
+  
+  title: {
+    textAlign: 'center',
+    marginTop: 13,
+    fontSize: 19,
+    color: 'white'
+  },
 })
